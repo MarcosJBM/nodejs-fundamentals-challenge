4fd10db80d39069ed0ccfa3b10eb5ab6519b358d
Marcos Botene <marcosjbotene@gmail.com>
2023-10-21 20:34:45 -0300

    feat: improving select method in database class
    creating a new object with the values in search containing only the properties that have values

--- src/database.js
+++ src/database.js
@@ -1,12 +1,39 @@
 import fs from 'node:fs';
 
-const databasePath = new URL('../db.json', import.meta.url);
+function filterSearchValues(search) {
+  let newSearch = {};
+
+  for (const key in search) {
+    if (search[key]) {
+      if (
+        typeof search[key] === 'string' ||
+        typeof search[key] === 'boolean' ||
+        typeof search[key] === 'number'
+      ) {
+        newSearch[key] = search[key];
+      }
+    }
+  }
+
+  return newSearch;
+}
+
+function hasPropertiesWithValues(object) {
+  for (const key in object) {
+    if (Object.prototype.hasOwnProperty.call(object, key) && object[key]) {
+      return true;
+    }
+  }
+
+  return false;
+}
 
 export class Database {
   #database = {};
+  #databasePath = new URL('../db.json', import.meta.url);
 
   constructor() {
-    fs.readFile(databasePath, 'utf8', (error, data) => {
+    fs.readFile(this.#databasePath, 'utf8', (error, data) => {
       if (error) return this.#persist();
 
       return (this.#database = JSON.parse(data));
@@ -14,7 +41,7 @@ export class Database {
   }
 
   #persist() {
-    fs.writeFile(databasePath, JSON.stringify(this.#database), error => {
+    fs.writeFile(this.#databasePath, JSON.stringify(this.#database), error => {
       if (error) console.error(error);
     });
   }
@@ -22,9 +49,14 @@ export class Database {
   select(table, search) {
     let data = this.#database[table] ?? [];
 
-    if (search) {
+    const filteredSearch = filterSearchValues(search);
+
+    const hasPropertiesWithValuesInSearch =
+      hasPropertiesWithValues(filteredSearch);
+
+    if (hasPropertiesWithValuesInSearch) {
       data = data.filter(row => {
-        return Object.entries(search).some(([key, value]) => {
+        return Object.entries(filteredSearch).some(([key, value]) => {
           return row[key].includes(value);
         });
       });
